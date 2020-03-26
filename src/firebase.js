3dbf5800ffed1aa571c29b8291fc09fa24c32d09
choosulyn <61719843+choosulyn@users.noreply.github.com>
2020-03-26 15:10:09 +0800

    linked to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
-
-  };
+  apiKey: "AIzaSyAHdOphsBlTBgASyQUkShjyfOpumJAmfQU",
+  authDomain: "bt3103-grocery-c92c1.firebaseapp.com",
+  databaseURL: "https://bt3103-grocery-c92c1.firebaseio.com",
+  projectId: "bt3103-grocery-c92c1",
+  storageBucket: "bt3103-grocery-c92c1.appspot.com",
+  messagingSenderId: "764869484450",
+  appId: "1:764869484450:web:be6c181e18c23c6fe35cfb",
+  measurementId: "G-D5GHVKMSBW"
+};
   
   firebase.initializeApp(firebaseConfig);
   var database = firebase.firestore();
